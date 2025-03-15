d8643df56a841d4891ebc80763ddaaf4b71af6da
Doan Minh Duy <duydoan812@gmail.com>
2025-03-15 10:40:51 +0700

    Finish Commit

--- Program.cs
+++ Program.cs
@@ -1,15 +1,43 @@
 ﻿Console.WriteLine("Linear Equation Resolver");
 Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+double a;
+double b;
 while (true)
 {
     Console.WriteLine("a :");
-    Console.WriteLine("b :");
-    if (double.TryParse(Console.ReadLine(), out double a) && (double.TryParse(Console.ReadLine(), out double b))
-    {
-        if ( a == 0 )
-
-
-    }
-
-
+    if (double.TryParse(Console.ReadLine(), out a))
+        break;
+    else Console.WriteLine("Please enter 'a' as a number ");
 }
+while (true)
+{
+    Console.WriteLine("b :");
+    if (double.TryParse(Console.ReadLine(), out b))
+        break;
+    else Console.WriteLine("Please enter 'b' as a number ");
+}
+if (a == 0)
+{
+    if (b == 0)
+    {
+        Console.WriteLine(" The solution is all x ");
+    }
+    else Console.WriteLine(" There is no solution ");
+}
+else
+{
+    if (b == 0)
+    {
+        Console.WriteLine(" The solution is 0 ");
+    }
+    else
+    {
+        double x = -b / a;
+        Console.WriteLine(" The solution is: " + x);
+    }
+}
+
+
+
+
+
