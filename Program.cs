500fe000b02284399517922d191434d6f7a0ee1e
Doan Minh Duy <duydoan812@gmail.com>
2025-03-15 09:40:27 +0700

    Add VS files commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Linear Equation Resolver");
+Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+while (true)
+{
+    Console.WriteLine("a :");
+    Console.WriteLine("b :");
+    if (double.TryParse(Console.ReadLine(), out double a) && (double.TryParse(Console.ReadLine(), out double b))
+    {
+        if ( a == 0 )
+
+
+    }
+
+
+}
